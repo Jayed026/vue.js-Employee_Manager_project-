d6c54a518c937e07e346acec2c5a46ed38aae175
jayed026 <bppyjyed@gmail.com>
2021-04-09 00:43:55 +0600

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDrmO1ZORw-0dWuBT1az4M2PiAcCWp2J0g",
+    authDomain: "vufs-pro.firebaseapp.com",
+    projectId: "vufs-pro",
+    storageBucket: "vufs-pro.appspot.com",
+    messagingSenderId: "340333026081",
+    appId: "1:340333026081:web:86dc7cb5f21ef3e907fc60",
+    measurementId: "G-5BFJTE8Y97"
+}
